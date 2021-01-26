96f2618b7692fb2fddf94c900b4606fabb203b80
Jennifer Bain <jennifer.fleming.bain@gmail.com>
2021-01-26 10:03:27 -0700

    incorporate markdown editor for creation of new journal entriess using react-markdown

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+import ReactMarkdown from "react-markdown";
+
+import MarkedInput from "./MarkedInput";
+const CreateMain = styled.main`
+  width: 100%;
+  border: 2px solid black;
+`;
+
+const EditorContainer = styled.div`
+  width: 100%;
+  border: 2px solid #b42828;
+`;
 
 const Create = () => {
+  const [markdownText, setMarkdownText] = useState("");
+
   return (
-    <div>
+    <CreateMain>
       <h1>TITLE</h1>
-      <p>Write here...</p>
-    </div>
+      <EditorContainer>
+        <MarkedInput setMarkdownText={setMarkdownText} />
+        <ReactMarkdown source={markdownText} />
+      </EditorContainer>
+    </CreateMain>
   );
 };
 
